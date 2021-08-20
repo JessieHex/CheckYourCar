8321657570d091a31c80adda834fb3ab8971a078
JessieHex <hexiaoju@deakin.edu.au>
2021-08-20 18:03:28 +0800

    Create CheckRecall Controller Create page of CheckRecall which read in car make and model and search for data in the database

---
+++ CheckYourCar/Controllers/CheckRecallController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheckYourCar.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CheckYourCar.Controllers
+{
+    public class CheckRecallController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        
+
+        [BindProperty]
+        public VehicleRecall VehicleRecall { get; set; }
+        public CheckRecallController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Check()
+        {
+            string make = HttpContext.Request.Form["MakeName"];
+            string model = HttpContext.Request.Form["ModelName"];
+            VehicleRecall = new VehicleRecall();
+            VehicleRecall = _db.VehicleRecalls.FirstOrDefault(u => u.MakeName == make && u.ModelName == model);
+
+            if (VehicleRecall == null)
+            {
+                return Content("Not found");
+            }
+
+            return Content("Recall message: " + VehicleRecall.Comment);// View(VehicleRecall); // TODO
+        }
+
+    }
+}
